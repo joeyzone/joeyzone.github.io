8b220069aa48d6fb29fb57b4e6b417f40cd3e675
Joey <joeyzoneone@gmail.com>
2023-09-12 08:23:41 +0800

    show address

--- src/components/ConnectWallet.vue
+++ src/components/ConnectWallet.vue
@@ -11,6 +11,7 @@
 <script lang="ts" setup>
 // import { ethers } from "ethers";
 import { ref, onMounted } from "vue";
+import { ethers } from "ethers";
 import { getAccount } from "@/typescript/connectWallet.ts";
 const text = ref("Connect Wallet");
 const connected = ref(false);
@@ -21,13 +22,22 @@ onMounted(async () => {
   const success = await getAccount();
   connected.value = success;
   if (connected.value) {
-    text.value = "Connected";
+    const addr = await getAddr();
+    text.value = addr;
   }
-  window.ethereum.on("accountsChanged", () => {
+  window.ethereum.on("accountsChanged", async () => {
+    const addr = await getAddr();
     connected.value = true;
-    text.value = "Connected";
+    text.value = addr;
   });
 });
+
+async function getAddr() {
+  const provider: any = new ethers.BrowserProvider(window.ethereum);
+  const signer = await provider.getSigner();
+  const addr = await signer.getAddress();
+  return `${addr.slice(0, 5)}...${addr.slice(-4)}`;
+}
 </script>
 <style lang="less" scoped>
 .connect-btn {
